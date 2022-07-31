f65babf27f47bf4b907984eb6e94e26e5553e8aa
Aryatama <18211338+aryatama045@users.noreply.github.com>
2022-07-31 16:10:16 +0700

    Update header.php

--- application/views/themes/takaeh/header.php
+++ application/views/themes/takaeh/header.php
@@ -4,6 +4,10 @@
     <title>TAKAEH PROPERTI</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
+    <meta name="author" content="http://www.takaeh.com/">
+    <meta name="robots" content="index, follow"/>
+    <meta name="google-site-verification" content="a0xUwqNBD0k0EvAJ6FmikJxvlGzU48gSVh4cIBU6xY4" />
+
     <!-- External CSS libraries -->
     <link type="text/css" rel="stylesheet" href="<?= base_url()?>themes/takaeh/css/bootstrap.min.css">
     <link type="text/css" rel="stylesheet" href="<?= base_url()?>themes/takaeh/css/magnific-popup.css">
@@ -28,6 +32,23 @@
     <!-- Custom Stylesheet -->
     <link type="text/css" rel="stylesheet" href="<?= base_url()?>themes/takaeh/css/style.css">
     <link rel="stylesheet" type="text/css" href="<?= base_url()?>themes/takaeh/css/skins/blue-light.css">
+
+    
+
+    <script data-ad-client="ca-pub-7766349185375988" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script src="https://www.googleoptimize.com/optimize.js?id=OPT-554G7SC"></script>
+    
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6RR4WDK0VN"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-6RR4WDK0VN');
+    </script>
 
 </head>
 <body id="top" class="index-body">
