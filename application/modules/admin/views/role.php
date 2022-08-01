c4f201a6718fb4180ea2816965a602025ceb5f85
Aryatama <18211338+aryatama045@users.noreply.github.com>
2022-08-01 15:03:04 +0700

    Update Role

--- application/modules/admin/views/role.php
+++ application/modules/admin/views/role.php
@@ -6,38 +6,42 @@
 
     <div class="row">
         <div class="col-lg-6">
-            <?= form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+            <div class="card mb-4">
+                <div class="card-body">
+                <?= form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
 
-            <?= $this->session->flashdata('message'); ?>
+                <?= $this->session->flashdata('message'); ?>
 
-            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newRoleModal">Add New Role</a>
+                <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newRoleModal">Add New Role</a>
 
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Role</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php $i = 1; ?>
-                    <?php foreach ($role as $r) : ?>
-                    <tr>
-                        <th scope="row"><?= $i; ?></th>
-                        <td><?= $r['role']; ?></td>
-                        <td>
-                            <a href="<?= base_url('admin/setting/roleaccess/') . $r['id']; ?>" class="badge badge-warning">access</a>
-                            <a href="" class="badge badge-success">edit</a>
-                            <a href="" class="badge badge-danger">delete</a>
-                        </td>
-                    </tr>
-                    <?php $i++; ?>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+                    <table class="data-table data-table-standard responsive nowrap">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Role</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $i = 1; ?>
+                            <?php foreach ($role as $r) : ?>
+                            <tr>
+                                <th scope="row"><?= $i; ?></th>
+                                <td><p><?= $r['role']; ?></p></td>
+                                <td>
+                                    <a href="<?= base_url('admin/setting/roleaccess/') . $r['id']; ?>"
+                                        class="badge badge-primary text-white">access</a>
+                                    <a href="" class="badge badge-success text-white">edit</a>
+                                    <a href="" class="badge badge-danger text-white">delete</a>
+                                </td>
+                            </tr>
+                            <?php $i++; ?>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
 
-
+                </div>
+            </div>
         </div>
     </div>
 
