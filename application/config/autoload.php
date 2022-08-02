45ba3757ce148da3c7de3dc17b5235ae1db14965
Aryatama <18211338+aryatama045@users.noreply.github.com>
2022-08-02 09:44:04 +0700

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('email', 'session', 'database','user_agent','form_validation','pagination', 'upload','uuid');
+$autoload['libraries'] = array('email', 'date', 'session', 'database','user_agent','form_validation','pagination', 'upload','uuid');
 
 
 /*
