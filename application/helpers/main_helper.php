6bc8db527c1a0aad568d1229bf8f50924d4bb72c
Aryatama <18211338+aryatama045@users.noreply.github.com>
2022-08-02 11:09:45 +0700

    Update main_helper.php

--- application/helpers/main_helper.php
+++ application/helpers/main_helper.php
@@ -14,111 +14,149 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 
-function is_logged_in()
-{
-    $ci = get_instance();
-    if (!$ci->session->userdata('email')) {
-        redirect('admin-login');
-    } else {
-        $role_id = $ci->session->userdata('role_id');
-        $url = uri_string(1);
-        $menu2 = ($ci->uri->segment(2));
-        // $menu2 = '/'.$menu[2];
+    function is_logged_in()
+    {
+        $ci = get_instance();
+        if (!$ci->session->userdata('email')) {
+            redirect('admin-login');
+        } else {
+            $role_id = $ci->session->userdata('role_id');
+            $url = uri_string(1);
+            $menu2 = ($ci->uri->segment(2));
+            // $menu2 = '/'.$menu[2];
 
-        $ci->db->like('url' , $url);
-        $queryMenu = $ci->db->get_where('menus')->row_array();
+            $ci->db->like('url' , $url);
+            $queryMenu = $ci->db->get_where('menus')->row_array();
 
-        // tesx($url, $menu2);
-        // $menu_id = $queryMenu['id'];
+            // tesx($url, $menu2);
+            // $menu_id = $queryMenu['id'];
 
-        $userAccess = $ci->db->get_where('user_access_menu', [
-            'role_id' => $role_id,
-            // 'menu_id' => $menu_id
-        ]);
+            $userAccess = $ci->db->get_where('user_access_menu', [
+                'role_id' => $role_id,
+                // 'menu_id' => $menu_id
+            ]);
 
-        if ($userAccess->num_rows() < 1) {
-            redirect('auth/blocked');
+            if ($userAccess->num_rows() < 1) {
+                redirect('auth/blocked');
+            }
         }
     }
-}
 
-function is_logged_agent()
-{
-    $ci = get_instance();
-    if (!$ci->session->userdata('email')) {
-        redirect('agent-login');
-    } else {
-        $role_id = $ci->session->userdata('role_id');
-        $url = uri_string(1);
-        $menu2 = ($ci->uri->segment(2));
-        // $menu2 = '/'.$menu[2];
+    function is_logged_agent()
+    {
+        $ci = get_instance();
+        if (!$ci->session->userdata('email')) {
+            redirect('agent-login');
+        } else {
+            $role_id = $ci->session->userdata('role_id');
+            $url = uri_string(1);
+            $menu2 = ($ci->uri->segment(2));
+            // $menu2 = '/'.$menu[2];
 
-        $ci->db->like('url' , $url);
-        $queryMenu = $ci->db->get_where('menus')->row_array();
+            $ci->db->like('url' , $url);
+            $queryMenu = $ci->db->get_where('menus')->row_array();
 
-        // tesx($url, $menu2);
-        // $menu_id = $queryMenu['id'];
+            // tesx($url, $menu2);
+            // $menu_id = $queryMenu['id'];
 
-        $userAccess = $ci->db->get_where('user_access_menu', [
-            'role_id' => $role_id,
-            // 'menu_id' => $menu_id
-        ]);
+            $userAccess = $ci->db->get_where('user_access_menu', [
+                'role_id' => $role_id,
+                // 'menu_id' => $menu_id
+            ]);
 
-        if ($userAccess->num_rows() < 1) {
-            redirect('auth/blocked');
+            if ($userAccess->num_rows() < 1) {
+                redirect('auth/blocked');
+            }
         }
     }
-}
 
 
-function check_access($role_id, $menu_id)
-{
-    $ci = get_instance();
+    function check_access($role_id, $menu_id)
+    {
+        $ci = get_instance();
 
-    $ci->db->where('role_id', $role_id);
-    $ci->db->where('menu_id', $menu_id);
-    $result = $ci->db->get('user_access_menu');
+        $ci->db->where('role_id', $role_id);
+        $ci->db->where('menu_id', $menu_id);
+        $result = $ci->db->get('user_access_menu');
 
-    if ($result->num_rows() > 0) {
-        return "checked='checked'";
-    }
-}
-
-function nominal($angka){
-    $jd = number_format($angka,2,',','.');
-    return $jd;
-}
-
-function format_indo($date){
-    date_default_timezone_set('Asia/Jakarta');
-    // array hari dan bulan
-    $Hari = array("Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu");
-    $Bulan = array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
-
-    // pemisahan tahun, bulan, hari, dan waktu
-    $tahun = substr($date,0,4);
-    $bulan = substr($date,5,2);
-    $tgl = substr($date,8,2);
-    $waktu = substr($date,11,5);
-    $hari = date("w",strtotime($date));
-    $result = $Hari[$hari];
-
-    return $result;
-}
-
-function tesx()
-{
-    $env = (ENVIRONMENT == 'production') ? 'none' : 'block';
-    $args = func_get_args();
-    if(is_array($args) && count($args)){ foreach($args as $x){
-        $echo = "<div style='display:$env'><pre>";
-        if(is_array($x) || is_object($x)){
-            $echo .= print_r($x, true);
-        }else{
-            $echo .= var_export($x, true);
+        if ($result->num_rows() > 0) {
+            return "checked='checked'";
         }
-        $echo .= "</pre><hr /></div>";
-        echo $echo;
-    }}
-    die();
-}
+    }
+
+    function nominal($angka){
+        $jd = number_format($angka,2,',','.');
+        return $jd;
+    }
+
+    function format_indo($date){
+        date_default_timezone_set('Asia/Jakarta');
+        // array hari dan bulan
+        $Hari = array("Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu");
+        $Bulan = array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
+
+        // pemisahan tahun, bulan, hari, dan waktu
+        $tahun = substr($date,0,4);
+        $bulan = substr($date,5,2);
+        $tgl = substr($date,8,2);
+        $waktu = substr($date,11,5);
+        $hari = date("w",strtotime($date));
+        $result = $Hari[$hari];
+
+        return $result;
+    }
+
+    function time_ago($time, $now)
+    {
+        $periods = array("detik", "menit", "jam", "hari", "minggu", "bulan", "tahun", "decade");
+        $lengths = array("60","60","24","7","4.35","12","10");
+
+        if($now == NULL){
+            $now = time();
+        }
+
+        $difference     = $now - $time;
+        $tense         = "ago";
+
+        for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+            $difference /= $lengths[$j];
+        }
+
+        $difference = round($difference);
+
+        if($difference != 1) {
+            $periods[$j].= "s";
+        }
+
+        return "$difference $periods[$j] ago ";
+    }
+
+    function selisih_jam($time, $now){
+
+        $diff  = $now-$time;
+        $jam   = floor($diff / (60 * 60));
+        $menit = $diff - ( $jam * (60 * 60) );
+        $detik = $diff % 60;
+
+        $result = $jam . ' Jam : ' . floor( $menit / 60 ) . ' Menit : ' .$detik. ' Detik';
+        // tesx($result);
+        return $result;
+    }
+
+
+    function tesx()
+    {
+        $env = (ENVIRONMENT == 'production') ? 'none' : 'block';
+        $args = func_get_args();
+        if(is_array($args) && count($args)){ foreach($args as $x){
+            $echo = "<div style='display:$env'><pre>";
+            if(is_array($x) || is_object($x)){
+                $echo .= print_r($x, true);
+            }else{
+                $echo .= var_export($x, true);
+            }
+            $echo .= "</pre><hr /></div>";
+            echo $echo;
+        }}
+        die();
+    }
