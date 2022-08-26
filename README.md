ab3143bb1b1302d4d290f90b00dc871a32cd2830
Aryatama <18211338+aryatama045@users.noreply.github.com>
2022-08-26 16:44:30 +0700

    Update

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # takaeh
  website master
 https://www.webslesson.info/2018/12/ajax-codeigniter-product-filter-with-pagination.html
-
-
+HXROG4WsLKox
--- application/modules/propertis/models/Properti_model.php
+++ application/modules/propertis/models/Properti_model.php
@@ -205,29 +205,47 @@ class Properti_model extends CI_Model
 			$countfiles = implode(' @$ ', $this->input->post('image'));
 			$imgaeview 	= explode(' @$ ', $countfiles);
 
+			// $imgaeview	= $this->input->post('image');
+			// $countfiles = count($this->input->post('image'));
+
 			// tesx($countfiles ,$imgaeview);
-			$no=0;
-			$result=array();
-			foreach ($imgaeview as $key => $val)
-			{
-				$image_parts 	= explode(";base64,", $val);
-				$image_type_aux = explode("image/", $image_parts[0]);
-				$image_type 	= $image_type_aux[0];
-				$image_base64 	= base64_decode($image_parts[0]);
-				$filename 		= $slug.'-'.time().'-thumb'.$no.'.jpg';
+
+			// foreach ($imgaeview as $key => $val)
+			$ximg = $this->input->post('image');
+			foreach($ximg as $key =>$val){
+
+
+				// if($this->is_base64($base64string) == true ){
+				// 	$base64string = "data:image/jpeg;base64,".$base64string;
+				// 	$this->check_size($base64string);
+				// 	$this->check_dir($path);
+				// 	$this->check_file_type($base64string);
+
+				// 	/*=================uploads=================*/
+				// 	list($type, $base64string) = explode(';', $base64string);
+				// 	list(,$extension)          = explode('/',$type);
+				// 	list(,$base64string)       = explode(',', $base64string);
+				// 	$fileName                  = uniqid().date('Y_m_d').'.'.$extension;
+				// 	$base64string              = base64_decode($base64string);
+				// 	file_put_contents($path.$fileName, $base64string);
+				// }
+				$base64string = "data:image/jpg;base64,".$val;
+				list($type, $base64string) = explode(';', $base64string);
+				list(,$extension)          = explode('/',$type);
+				list(,$base64string)       = explode(',', $base64string);
+				$filename     = $slug.'-'.time().'-thumb.'.$extension;
+				$base64       = base64_decode($base64string);
+
+				// $img = str_replace('data:image/png;base64,', '', $val);
+				// $img = str_replace(' ', '+', $img);
+				// $base64 = base64_decode($img);
+				// $filename     = $slug.'-'.time().'-thumb.png';
+
 				$file 			= FCPATH.'/www/properties/sliders/'.$filename;
-				file_put_contents($file, $image_base64);
+				file_put_contents($file, $base64);
 
-				array_push($result, $filename);
-				// $img = str_replace('[removed]', '', $val);
-				// $encoded = str_replace('[removed]', 'data:image/png;base64,', $val);
 
-				// // tesx($val, $encoded);
-				// $decoded = base64_decode($encoded);
-				// $filename 		= $slug.'-'.time().'-thumb.png';
-				// $path 			= FCPATH.'/www/properties/sliders/'.$filename;
-
-				// file_put_contents($path,$decoded);
+				// array_push($result, $filename);
 
 				$imageData = array(
 					'id_properties' => $id_properties,
@@ -235,52 +253,51 @@ class Properti_model extends CI_Model
 					'properties_url' => $photo_cover,
 				);
 				// $this->db->insert('properties_slider',$imageData);
-				$no++;
 			}
 
-			tesx($result, $imgaeview);
+			tesx($ximg);
 			// $result = array();
-			// for ($i = 0; $i < $filesCount; ) {
+				// for ($i = 0; $i < $filesCount; ) {
 
-			// 	$ImgThumb = $ImgData[$i];
-			// 	$img = str_replace('data:image/png;base64,', '', $ImgThumb);
-			// 	$encoded = str_replace(' ', '+', $img);
-			// 	$decoded = base64_decode($encoded);
+				// 	$ImgThumb = $ImgData[$i];
+				// 	$img = str_replace('data:image/png;base64,', '', $ImgThumb);
+				// 	$encoded = str_replace(' ', '+', $img);
+				// 	$decoded = base64_decode($encoded);
 
-			// 	$path = FCPATH.'/www/properties/sliders/'.$slug[$i].'-'.+'1'.'-thumb.png';
+				// 	$path = FCPATH.'/www/properties/sliders/'.$slug[$i].'-'.+'1'.'-thumb.png';
 
-			// 	file_put_contents($path,$decoded);
+				// 	file_put_contents($path,$decoded);
 
-			// 	array_push($result, $ImgThumb);
-			// 	$type 		= strtolower($_FILES['userfile2']['name'][$i]);
+				// 	array_push($result, $ImgThumb);
+				// 	$type 		= strtolower($_FILES['userfile2']['name'][$i]);
 
-			// 	$name_sub 	= str_replace(' ', '-', strtolower($type));
+				// 	$name_sub 	= str_replace(' ', '-', strtolower($type));
 
-			// 	$_FILES['file']['name'] 	= $slug.'-'.+'1'.'-'.time().'-'.$name_sub;
-			// 	$_FILES['file']['type'] 	= $_FILES['userfile2']['type'][$i];
-			// 	$_FILES['file']['tmp_name']	= $_FILES['userfile2']['tmp_name'][$i];
-			// 	$_FILES['file']['error'] 	= $_FILES['userfile2']['error'][$i];
-			// 	$_FILES['file']['size']		= $_FILES['userfile2']['size'][$i];
+				// 	$_FILES['file']['name'] 	= $slug.'-'.+'1'.'-'.time().'-'.$name_sub;
+				// 	$_FILES['file']['type'] 	= $_FILES['userfile2']['type'][$i];
+				// 	$_FILES['file']['tmp_name']	= $_FILES['userfile2']['tmp_name'][$i];
+				// 	$_FILES['file']['error'] 	= $_FILES['userfile2']['error'][$i];
+				// 	$_FILES['file']['size']		= $_FILES['userfile2']['size'][$i];
 
-			// 	// File upload configuration
-			// 	$uploadPath = FCPATH.'/www/properties/sliders/';
-			// 	$configs['upload_path'] = $uploadPath;
-			// 	$configs['allowed_types'] = 'jpg|jpeg|png|gif';
+				// 	// File upload configuration
+				// 	$uploadPath = FCPATH.'/www/properties/sliders/';
+				// 	$configs['upload_path'] = $uploadPath;
+				// 	$configs['allowed_types'] = 'jpg|jpeg|png|gif';
 
 
-			// 	// Load and initialize upload library
-			// 	$this->load->library('upload', $configs);
-			// 	$this->upload->initialize($configs);
+				// 	// Load and initialize upload library
+				// 	$this->load->library('upload', $configs);
+				// 	$this->upload->initialize($configs);
 
-			// 	// Upload file to server
-			// 	if ($this->upload->do_upload('file')) {
-			// 		// Uploaded file data
-			// 		$fileData = $this->upload->data();
-			// 		$ImgData[$i]['id_properties'] 	= $id_properties;
-			// 		$ImgData[$i]['photo_slider'] 	= $slug.'-'.+'1'.'-'.time().'-'.$name_sub ;
-			// 		$ImgData[$i]['properties_url'] 	= $photo_cover;
-			// 	}
-			// }
+				// 	// Upload file to server
+				// 	if ($this->upload->do_upload('file')) {
+				// 		// Uploaded file data
+				// 		$fileData = $this->upload->data();
+				// 		$ImgData[$i]['id_properties'] 	= $id_properties;
+				// 		$ImgData[$i]['photo_slider'] 	= $slug.'-'.+'1'.'-'.time().'-'.$name_sub ;
+				// 		$ImgData[$i]['properties_url'] 	= $photo_cover;
+				// 	}
+				// }
 			// $this->master->insert_batch('properties_slider', $ImgData);
 		/* --Add Item Slider --*/
 
@@ -288,6 +305,44 @@ class Properti_model extends CI_Model
 
 		return ($nama_rumah)?$nama_rumah:false;
 	}
+
+	function is_base64($s){
+        // Check if there are valid base64 characters
+       // if (!preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $s)) return false;
+        // Decode the string in strict mode and check the results
+        $decoded = base64_decode($s, true);
+        if(false === $decoded) return false;
+        // Encode the string again
+        if(base64_encode($decoded) != $s) return false;
+        return true;
+    }
+
+	public function check_size($base64string){
+        $file_size = 8000000;
+        $size = @getimagesize($base64string);
+        if($size['bits'] >= $file_size){
+            print_r(json_encode(array('status' =>false,'message' => 'file size not allowed !')));exit;
+        }
+        return true;
+    }
+
+    public function check_dir($path){
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
+            return true;
+        }
+        return true;
+    }
+
+    public function check_file_type($base64string){
+        $mime_type = @mime_content_type($base64string);
+        $allowed_file_types = ['image/png', 'image/jpeg', 'application/pdf'];
+        if (! in_array($mime_type, $allowed_file_types)) {
+            // File type is NOT allowed
+           // print_r(json_encode(array('status' =>false,'message' => 'File type is NOT allowed !')));exit;
+        }
+        return true;
+    }
 
 	public function removeProperti()
 	{
