a2553bb8cb3fd514d420b230d298e70a02455057
Aryatama <18211338+aryatama045@users.noreply.github.com>
2022-09-26 16:43:52 +0700

    Create profile.php

---
+++ application/views/takaeh/agents/profile.php
@@ -0,0 +1,240 @@
+<!-- Sub banner start -->
+<div class="sub-banner">
+    <div class="container">
+        <div class="breadcrumb-area">
+            <h1>My Properties</h1>
+            <ul class="breadcrumbs">
+                <li><a href="index.html">Home</a></li>
+                <li class="active">My Properties</li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- Sub banner end -->
+
+<!-- User page start -->
+<div class="user-page content-area-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-12 col-sm-12">
+                <div class="user-profile-box mrb">
+                    <!--header -->
+                    <div class="header clearfix">
+                        <h2>Emma Connor</h2>
+                        <h4>Creative Director</h4>
+                        <img src="http://placehold.it/160x160" alt="avatar" class="img-fluid profile-img">
+                    </div>
+                    <!-- Detail -->
+                    <div class="detail clearfix">
+                        <ul>
+                            <li>
+                                <a href="user-profile.html">
+                                    <i class="flaticon-user"></i>Profile
+                                </a>
+                            </li>
+                            <li>
+                                <a href="my-properties.html" class="active">
+                                    <i class="flaticon-house"></i>My Properties
+                                </a>
+                            </li>
+                            <li>
+                                <a href="favorited-properties.html">
+                                    <i class="flaticon-heart-shape-outline"></i>Favorited Properties
+                                </a>
+                            </li>
+                            <li>
+                                <a href="submit-property.html">
+                                    <i class="flaticon-add"></i>Submit New Property
+                                </a>
+                            </li>
+                            <li>
+                                <a href="change-password.html">
+                                    <i class="flaticon-locked-padlock"></i>Change Password
+                                </a>
+                            </li>
+                            <li>
+                                <a href="index.html">
+                                    <i class="flaticon-logout"></i>Log Out
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8 col-md-12 col-sm-12">
+                <div class="my-properties">
+                    <table class="manage-table">
+                        <thead>
+                        <tr>
+                            <th>My Properties</th>
+                            <th></th>
+                            <th>Date</th>
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody class="responsive-table">
+                        <tr>
+                            <td class="listing-photoo">
+                                <img alt="my-properties" src="http://placehold.it/163x108" class="img-fluid">
+                            </td>
+                            <td class="title-container">
+                                <h5><a href="#">Real Luxury Villa</a></h5>
+                                <h6><span>$90.00</span> / monthly</h6>
+                                <p><i class="flaticon-facebook-placeholder-for-locate-places-on-maps"></i> 123 Kathal St. Tampa City, </p>
+                            </td>
+                            <td class="date">
+                                7.02.2020
+                            </td>
+                            <td class="action">
+                                <ul>
+                                    <li>
+                                        <a href="#"><i class="fa fa-pencil"></i> Edit</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="fa  fa-eye-slash"></i> Hide</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="delete"><i class="fa fa-remove"></i> Delete</a>
+                                    </li>
+                                </ul>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <table class="manage-table">
+                        <tbody>
+                        <tr>
+                            <td class="listing-photoo">
+                                <img alt="my-properties" src="http://placehold.it/163x108" class="img-fluid">
+                            </td>
+                            <td class="title-container">
+                                <h5><a href="#">Luxury Villa</a></h5>
+                                <h6><span>$90.00</span> / monthly</h6>
+                                <p><i class="flaticon-facebook-placeholder-for-locate-places-on-maps"></i> 123 Kathal St. Tampa City, </p>
+                            </td>
+                            <td class="date">
+                                7.02.2020
+                            </td>
+                            <td class="action">
+                                <ul>
+                                    <li>
+                                        <a href="#"><i class="fa fa-pencil"></i> Edit</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="fa  fa-eye-slash"></i> Hide</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="delete"><i class="fa fa-remove"></i> Delete</a>
+                                    </li>
+                                </ul>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <table class="manage-table">
+                        <tbody>
+                        <tr>
+                            <td class="listing-photoo">
+                                <img alt="my-properties" src="http://placehold.it/163x108" class="img-fluid">
+                            </td>
+                            <td class="title-container">
+                                <h5><a href="#">Relaxing Apartment</a></h5>
+                                <h6><span>$90.00</span> / monthly</h6>
+                                <p><i class="flaticon-facebook-placeholder-for-locate-places-on-maps"></i> 123 Kathal St. Tampa City, </p>
+                            </td>
+                            <td class="date">
+                                7.02.2020
+                            </td>
+                            <td class="action">
+                                <ul>
+                                    <li>
+                                        <a href="#"><i class="fa fa-pencil"></i> Edit</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="fa  fa-eye-slash"></i> Hide</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="delete"><i class="fa fa-remove"></i> Delete</a>
+                                    </li>
+                                </ul>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <table class="manage-table">
+                        <tbody>
+                        <tr>
+                            <td class="listing-photoo">
+                                <img alt="my-properties" src="http://placehold.it/163x108" class="img-fluid">
+                            </td>
+                            <td class="title-container">
+                                <h5><a href="#">Luxury Villa</a></h5>
+                                <h6><span>$90.00</span> / monthly</h6>
+                                <p><i class="flaticon-facebook-placeholder-for-locate-places-on-maps"></i> 123 Kathal St. Tampa City, </p>
+                            </td>
+                            <td class="date">
+                                7.02.2020
+                            </td>
+                            <td class="action">
+                                <ul>
+                                    <li>
+                                        <a href="#"><i class="fa fa-pencil"></i> Edit</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="fa  fa-eye-slash"></i> Hide</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="delete"><i class="fa fa-remove"></i> Delete</a>
+                                    </li>
+                                </ul>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <table class="manage-table mb-30">
+                        <tbody>
+                        <tr>
+                            <td class="listing-photoo">
+                                <img alt="my-properties" src="http://placehold.it/163x108" class="img-fluid">
+                            </td>
+                            <td class="title-container">
+                                <h5><a href="#">Relaxing Apartment</a></h5>
+                                <h6><span>$90.00</span> / monthly</h6>
+                                <p><i class="flaticon-facebook-placeholder-for-locate-places-on-maps"></i> 123 Kathal St. Tampa City, </p>
+                            </td>
+                            <td class="date">
+                                7.02.2020
+                            </td>
+                            <td class="action">
+                                <ul>
+                                    <li>
+                                        <a href="#"><i class="fa fa-pencil"></i> Edit</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="fa  fa-eye-slash"></i> Hide</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="delete"><i class="fa fa-remove"></i> Delete</a>
+                                    </li>
+                                </ul>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="pagination-box text-center">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <li class="page-item"><a class="page-link" href="#"><span aria-hidden="true">«</span></a></li>
+                            <li class="page-item"><a class="page-link active" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#"><span aria-hidden="true">»</span></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- User page end -->
