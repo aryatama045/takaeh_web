4f42a58bcac0943a5e688c91a5e876cbb4f4c84d
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-01-09 13:46:14 +0700

    Update index.php

--- application/modules/feature/views/email/index.php
+++ application/modules/feature/views/email/index.php
@@ -354,7 +354,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
-<script type="text/javascript">
+<script>
     var base_url = '<?php echo base_url() ?>';
 
 
